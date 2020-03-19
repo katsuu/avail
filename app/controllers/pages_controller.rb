9bbbdf452fdadec42ace423f02b7c3adc5b62ee7
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-19 20:39:14 +0000

    build out hangouts controller crud with placeholder routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-
+    # ACCESS TO AVAILBILITY
+    # WAY TO START NEW HANGOUT
   end
 end
