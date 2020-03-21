ced9f1171bbf89d9b13ec8f7d1c92626fe9cce7a
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-21 15:41:11 +0000

    create map partial, js files, addtl mapbox config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,10 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+/////////////// MAPBOX ///////////////
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
