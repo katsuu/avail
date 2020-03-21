06afe4d6f3a1121a4a283e26312dca421b9faa7a
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-21 14:12:12 +0000

    gem added, finish basic config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def user_location
+    @user_location = Geocoder.search(request.remote_ip).first.coordinates
+  end
 end
